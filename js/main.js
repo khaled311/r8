6a96def25643e7a5a0733a63ef7c3c5339ad0a88
Khaled <khaled@Khaleds-MacBook-Pro.local>
2020-08-27 05:26:58 +0200

    Edit

--- js/main.js
+++ js/main.js
@@ -265,27 +265,18 @@ $(function () {
         cursorwidth: "0px",
     });
 
-    $(".products-popup").niceScroll({
-        cursorwidth: "8px",
-        cursorcolor: "#244d75",
-        background: "#d1a42e",
-        cursorborder: "none",
-        cursorborderradius: 0,
-    });
-
-    $(".products-popup .slidaya").on("mouseenter", function (e) {
+    $(".products-popup .offer-box").on("click", function (e) {
         e.preventDefault();
         $(this)
-            .find(".offer-box")
             .addClass("slide")
             .next(".pro-slide")
             .addClass("slide")
             .slideDown();
     });
 
-    $(".products-popup .slidaya").on("mouseleave", function () {
-        $(this).find(".pro-slide").removeClass("slide").slideUp();
-        $(this).find(".offer-box").removeClass("slide");
+    $(".pro-slide > .pic").on("click", function () {
+        $(this).parent().removeClass("slide").slideUp();
+        $(this).parent().parent().find(".offer-box").removeClass("slide");
     });
 
     jQuery("img.svg").each(function () {
